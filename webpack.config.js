49be42836a0dcffe0f6260e6de87c487a6f0c88f
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-18 15:05:53 +0200

    Fix path for app.bundle.jsin webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: './build/app.bundle.js'
+    filename: './app.bundle.js'
   },
 
   module: {
