3c67c9aa032fffcff1a9c8a394ac154c7be5d041
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-18 14:45:02 +0200

    Instal loaders for css files, add config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,19 @@ module.exports = {
       {
         test: /\.js$/,
         loader: "babel-loader"
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
       }
     ]
   }
