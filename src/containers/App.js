87710c0cb4b30f2f4ca896f26604c2c3b4063fbc
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-19 18:03:48 +0200

    Add module Title.js.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title.js';
 
 class App extends React.Component {
   constructor(props){
@@ -27,7 +28,9 @@ class App extends React.Component {
   render() {
     return (
       <div className={style.TodoApp}>
-        Tutaj pojawią się komponenty naszej aplikacji.
+        <Title
+          todoItemsArray={this.state.data}
+        />
       </div>
     );
   }
